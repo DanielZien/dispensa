86b66bc6e4da9535af821d208c0324cc23015aaa
DanielZien <daniel.s@sou.ufac.br>
2025-12-08 12:34:38 -0500

    feat: tem que arrumar

---
+++ Dispensa/src/main/java/com/daniel/Dispensa/DispensaApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.Dispensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispensaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispensaApplication.class, args);
+	}
+
+}
---
+++ Dispensa/src/test/java/com/daniel/Dispensa/DispensaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.Dispensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispensaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
