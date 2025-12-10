ed97c9d709e5c46c9993519eea616fffba6319ec
DanielZien <daniel.s@sou.ufac.br>
2025-12-10 13:30:43 -0500

    feat

--- Dispensa/src/main/java/com/daniel/Dispensa/Controller/ItemDepensaController.java
+++ Dispensa/src/main/java/com/daniel/Dispensa/Controller/ItemDepensaController.java
@@ -2,8 +2,11 @@ package com.daniel.Dispensa.Controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +31,17 @@ public class ItemDepensaController {
     }
     @PostMapping
     public ItemDespensa create(@RequestBody ItemDespensa item) {
-        return itemService.save(item);
+        return itemService.salve(item);
+    }
+     @PutMapping("/{id}")
+    public ItemDespensa update(@PathVariable Long id, @RequestBody ItemDespensa itemAtualizado) {
+        return itemService.atualizarItem(id, itemAtualizado);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        itemService.delete(id);
     }
 
 }
--- Dispensa/src/main/java/com/daniel/Dispensa/Service/ItemService.java
+++ Dispensa/src/main/java/com/daniel/Dispensa/Service/ItemService.java
@@ -21,9 +21,22 @@ public class ItemService {
     }
 
     //CRIAR
-    public ItemDespensa save(ItemDespensa item){
+    public ItemDespensa salve(ItemDespensa item){
         return itemRepository.save(item);
     }
+    //ATUALIZAR
+    public ItemDespensa atualizarItem(Long id, ItemDespensa itemAtualizado) {
+        ItemDespensa existente = itemRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Item não encontrado"));
+
+        existente.setNome(itemAtualizado.getNome());
+        existente.setDataValidade(itemAtualizado.getDataValidade());
+        existente.setQuantidade(itemAtualizado.getQuantidade());
+        existente.setLocalizacao(itemAtualizado.getLocalizacao());
+
+        return itemRepository.save(existente);
+    }
+
     //DELETAR
     public void delete(Long Id){
         itemRepository.deleteById(Id);
